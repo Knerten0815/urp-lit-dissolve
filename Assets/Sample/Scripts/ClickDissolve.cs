bc4c278094af1377bb3c8c6f52b6e034d269d8d1
Kevin Zielke <kevin_zielke@yahoo.de>
2025-11-18 17:42:05 +0100

    - Added ClickDissolve interaction. - Added Dissolve Origin Gizmo.

---
+++ Assets/Sample/Scripts/ClickDissolve.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using UnityEngine;
+
+public class ClickDissolve : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _meshRenderer;
+    [SerializeField] private float _maxRadius = 2;
+    [SerializeField] private float _dissolveSpeed = 1;
+    private Material _dissolveMaterial;
+    private float _resetRadius = 0;
+
+    private void Awake()
+    {
+        if (_meshRenderer == null)
+            _meshRenderer = GetComponent<MeshRenderer>();
+
+        _dissolveMaterial = _meshRenderer.sharedMaterial;
+
+        if (_dissolveMaterial.shader != Shader.Find("Universal Render Pipeline/LitDissolve"))
+            _dissolveMaterial = null;
+        else
+        {
+            _resetRadius = _dissolveMaterial.GetFloat("_DissolveRadius");
+            _dissolveMaterial.SetFloat("_DissolveRadius", 0);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) == false)
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit) == false)
+            return;
+
+        if (hit.collider.gameObject == this.gameObject)
+        {
+            StopAllCoroutines();
+            StartCoroutine(DissolveCoroutine(hit.point));
+        }
+    }
+
+    private IEnumerator DissolveCoroutine(Vector3 origin)
+    {
+        if (_dissolveMaterial == null)
+            yield break;
+
+        float radius = 0;
+
+        _dissolveMaterial.SetFloat("_DissolveRadius", radius);
+        _dissolveMaterial.SetVector("_DissolveOrigin", new Vector4(origin.x, origin.y, origin.z));
+
+        while (radius < _maxRadius)
+        {
+            yield return null;
+
+            radius += Time.deltaTime * _dissolveSpeed;
+            _dissolveMaterial.SetFloat("_DissolveRadius", radius);
+        }
+
+        while (radius > 0)
+        {
+            yield return null;
+
+            radius -= Time.deltaTime * _dissolveSpeed;
+            _dissolveMaterial.SetFloat("_DissolveRadius", radius);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _dissolveMaterial.SetFloat("_DissolveRadius", _resetRadius);
+    }
+}
---
+++ Assets/Sample/Scripts/DissolveGizmo.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using UnityEngine;
+
+public class DissolveGizmo : MonoBehaviour
+{
+    [SerializeField] GameObject _gizmoBase, _arrowX, _arrowY, _arrowZ, _radiusIndicator;
+    [SerializeField] float _scaleMultiplier = 0.01f;
+    [SerializeField] private Material _matX, _matY, _matZ, _matBase;
+    [SerializeField] private Material[] _dissolveMaterials;
+    private bool isDragging = false;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Check if we are hovering gizmo or radius indicator. Reset visuals if not
+        if (Physics.Raycast(ray, out RaycastHit hitGizmos, maxDistance: float.MaxValue, LayerMask.GetMask("Gizmos")) == false
+            && Physics.Raycast(ray, out RaycastHit hitIndicator, maxDistance: float.MaxValue, LayerMask.GetMask("Indicator")) == false)
+        {
+            ResetVisuals();
+            return;
+        }
+
+        // only continue if gizmos were hit
+        if (hitGizmos.collider == null)
+            return;
+
+        // get material of the arrow/base and highlight that part.
+        Material hitMaterial = hitGizmos.collider.GetComponent<MeshRenderer>().sharedMaterial;
+        Hover(hitMaterial);
+
+        if (Input.GetMouseButtonDown(0) == false)
+            return;
+
+        UpdateDissolveOrigin();
+        UpdateDissolveRadius();
+
+        if (hitMaterial == _matBase)
+        {
+            StartCoroutine(ScaleDrag());
+        }
+        else
+        {
+            StartCoroutine(PositionDrag(hitMaterial));
+        }
+    }
+
+
+    
+    private void ResetVisuals()
+    {
+        if (isDragging)
+            return;
+        
+        _matX.color = DimColor(Color.red);
+        _matY.color = DimColor(Color.green);
+        _matZ.color = DimColor(Color.blue);
+        _matBase.color = DimColor(Color.white);
+
+        _arrowX.SetActive(false);
+        _arrowY.SetActive(false);
+        _arrowZ.SetActive(false);
+        _radiusIndicator.SetActive(false);
+    }
+
+    private void Hover(Material material)
+    {
+        if (isDragging)
+            return;
+        
+        _matX.color = material == _matX ? Color.red : DimColor(Color.red);
+        _matY.color = material == _matY ? Color.green : DimColor(Color.green);
+        _matZ.color = material == _matZ ? Color.blue : DimColor(Color.blue);
+        _matBase.color = material == _matBase ? Color.white : DimColor(Color.white);
+
+        _arrowX.SetActive(true);
+        _arrowY.SetActive(true);
+        _arrowZ.SetActive(true);
+        _radiusIndicator.SetActive(true);
+    }
+
+    private Color DimColor(Color color)
+    {
+        color *= 0.5f;
+        color.a = 1;
+        return color;
+    }
+
+    private void UpdateDissolveOrigin()
+    {
+        foreach (Material mat in _dissolveMaterials)
+            mat.SetVector("_DissolveOrigin", transform.position);
+    }
+
+    private void UpdateDissolveRadius()
+    {
+        foreach (Material mat in _dissolveMaterials)
+            mat.SetFloat("_DissolveRadius", _radiusIndicator.transform.localScale.x * 0.5f);
+    }
+
+    private Plane GetPlane(Material material)
+    {
+        if (material == _matX || material == _matY)
+            return new Plane(Vector3.forward, transform.position.z);
+
+        if (material == _matZ)
+            return new Plane(Vector3.right, transform.position.x);
+
+        return new Plane();
+    }
+    
+    private Vector3 ProjectMouseToWorldPosition(Plane plane)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        plane.Raycast(ray, out float distanceToPlane);
+        return ray.GetPoint(distanceToPlane);
+    }
+
+    private IEnumerator PositionDrag(Material hitMaterial)
+    {
+        isDragging = true;
+
+        Plane plane = GetPlane(hitMaterial);
+        Vector3 currentPos = ProjectMouseToWorldPosition(plane);
+        Vector3 lastPos = currentPos;
+        Vector3 worldDir;
+        Vector3 moveDir;
+
+        while (Input.GetMouseButton(0))
+        {
+            currentPos = ProjectMouseToWorldPosition(plane);
+            worldDir = currentPos - lastPos;
+            moveDir = Vector3.zero;
+
+            if (hitMaterial == _matX)
+                moveDir = Vector3.right * worldDir.x;
+            else if (hitMaterial == _matY)
+                moveDir = Vector3.up * worldDir.y;
+            else if (hitMaterial == _matZ)
+                moveDir = Vector3.forward * worldDir.z;
+
+            transform.position -= moveDir;
+            UpdateDissolveOrigin();
+
+            lastPos = currentPos;
+            yield return null;
+        }
+
+        isDragging = false;
+    }
+
+    private IEnumerator ScaleDrag()
+    {
+        isDragging = true;
+
+        Vector3 startMousePos = Input.mousePosition;
+        Vector3 startScale = _radiusIndicator.transform.localScale;
+        Vector3 currentMousePos;
+        Vector3 newScale;
+        float diff;
+
+        while (Input.GetMouseButton(0))
+        {
+            currentMousePos = Input.mousePosition;
+            diff = (currentMousePos - startMousePos).y;
+            newScale = startScale + Vector3.one * (diff * _scaleMultiplier);
+
+            if (newScale.x <= _gizmoBase.transform.localScale.x)
+            {
+                yield return null;
+                continue;
+            }
+
+            _radiusIndicator.transform.localScale = newScale;
+            UpdateDissolveRadius();
+
+            yield return null;
+        }
+
+        isDragging = false;
+    }
+}
